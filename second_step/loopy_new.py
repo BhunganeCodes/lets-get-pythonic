4fb80fa5050315ad11f589e55bfa79ef8b9c168c
Thamsanqa Hadebe <thamsanqahadebe98@gmail.com>
2026-02-15 14:16:18 +0200

    solved: grade analyzer

--- second_step/loopy_new.py
+++ second_step/loopy_new.py
@@ -19,7 +19,7 @@ def temperature_converter(celsius_temps):
         res.append(int(c * 9/5) + 32)
     
     return res
-print(temperature_converter([0, 100, -40]))
+
 
 def password_strength():
     
@@ -28,7 +28,14 @@ def password_strength():
 
 
 def grade_analyzer(grades):
-    pass
+    if not grades:
+        return "No Pass"
+
+    for g in grades:
+        if g < 60:
+            return "No Pass"
+    
+    return "Pass"
 
 
 def valley_finder(data):
